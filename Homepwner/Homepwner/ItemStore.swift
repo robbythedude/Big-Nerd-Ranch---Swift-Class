63eb1dd0f5e215ba168ed89e7bc0682d3ad5a679
Robert Steiner <brogrammerrob@gmail.com>
2017-09-13 08:17:19 -0700

    Chapter Nine code changes for Homepwner app

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,47 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Robert Steiner on 9/12/17.
+//  Copyright © 2017 Robert Steiner. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore{
+    var allItems = [Item]()
+    
+    init(){
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    func removeItem(_ item: Item){
+        if let index = allItems.index(of: item){
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int){
+        if fromIndex == toIndex {
+            return
+        }
+        //Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        //Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        //Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
+}
