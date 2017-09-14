f4599766d800d59e8a186cbed4d5519a344e6284
Robert Steiner <brogrammerrob@gmail.com>
2017-09-14 06:27:56 -0700

    Rest of Chapter Thirteen code changes for Homepwner App

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -11,10 +11,20 @@ import UIKit
 class ItemStore{
     var allItems = [Item]()
     
+    let itemArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.archive")
+    }()
+    
     init(){
-        for _ in 0..<5 {
-            createItem()
+//        for _ in 0..<5 {
+//            createItem()
+//        }
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems = archivedItems
         }
+        
     }
     
     @discardableResult func createItem() -> Item {
@@ -42,6 +52,11 @@ class ItemStore{
         
         //Insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
     }
     
 }
