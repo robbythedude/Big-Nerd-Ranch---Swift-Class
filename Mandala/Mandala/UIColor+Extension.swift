a4b29278c5402d8b716fa67309232ee8720d84d8
Robert Steiner <brogrammerrob@gmail.com>
2017-09-14 14:19:27 -0700

    Chapter Seventeen  code changes for the Mandala app

---
+++ Mandala/Mandala/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  Mandala
+//
+//  Created by Robert Steiner on 9/14/17.
+//  Copyright © 2017 Robert Steiner. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
+}
